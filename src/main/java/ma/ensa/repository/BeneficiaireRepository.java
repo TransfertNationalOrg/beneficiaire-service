389147aac85151d57c751fcef13bb8867019afb8
ambou-97 <ambroise.tine@gmail.com>
2022-01-12 14:32:03 +0100

    adding @RepositoryRestResource

--- src/main/java/ma/ensa/repository/BeneficiaireRepository.java
+++ src/main/java/ma/ensa/repository/BeneficiaireRepository.java
@@ -1,7 +1,9 @@
 package ma.ensa.repository;
 import ma.ensa.model.Beneficiaire;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface BeneficiaireRepository extends JpaRepository<Beneficiaire, Long> {
 
 }
